3c69ba314b7ac9cb3f52c9708e333abce19d238a
KAR <151836077+ikunalar2609@users.noreply.github.com>
2025-05-28 10:21:07 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,4 +1,4 @@
-/* import { Button } from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
 import { ArrowDown, ArrowRight, BarChart3, Target, LineChart, Mail, Instagram, Twitter, Linkedin, BookOpen, Leaf } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
